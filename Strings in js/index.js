a56dd024aa1276b47281cd9036b07697f210d521
Husnain <husnainali41940@gmail.com>
2025-07-23 00:34:13 -0700

    strings in js

---
+++ Strings in js/index.js
@@ -0,0 +1,26 @@
+// By double quote
+let name = "Lukas"
+console.log(name)
+
+// By single quote
+let medicine = 'Denzen-ds'
+// console.log(medicine.length)
+console.log(medicine[3])
+
+// Template Literals
+let boy1= "Umair"
+let boy2= "Waddod" 
+let sentence =`${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+// Escape sequence
+let fruit = 'Bana\'na';
+console.log(fruit); // Output: Bana'na
+
+let str1 = 'It\'s a nice day';
+let str2 = "He said, \"Hello!\"";
+
+console.log(str1); // Output: It's a nice day
+console.log(str2); // Output: He said, "Hello!"
+
+
