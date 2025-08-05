df178040df7f8aceb3e56db0a37881e1ded1331e
Husnain <husnainali41940@gmail.com>
2025-08-04 23:26:24 -0700

    Promise  Api done

--- Promise Chaining/index.js
+++ Promise Chaining/index.js
@@ -1,18 +1,32 @@
-let p1=new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-console.log("Resolved after 5 seconds")
-resolve(56)
-    },5000)
-})
-p1.then((value)=>{
-    console.log(value)
-    let p2=new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            resolve("Promise 2")
-        }, 2000)
+// let p1=new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+// console.log("Resolved after 5 seconds")
+// resolve(56)
+//     },5000) 
+// })
+// p1.then((value)=>{
+//     console.log(value)
+//     let p2=new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             resolve("Promise 2")
+//         }, 2000)
     
-    })
-    return p2
-}).then((value)=>{
-    conole.log("we are done")
+//     })
+//     return p2
+// }).then((value)=>{
+//     conole.log("we are done")
+// })
+
+// Attaching handlers
+let p1=new Promise ((resolve, reject)=> {
+    setTimeout(()=>{
+// alert("Hey i am not resolved")     
+        resolve(1);
+    },2000)
+})
+p1.then(()=>{
+    console.log("Congratulations this promise is now resolved")
+})
+p1.then(()=>{
+    console.log("Hurray")
 })
